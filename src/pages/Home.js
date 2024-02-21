0fd1d2667e10a67cfd29f3830661552f24a7a6fb
StyngerBee <cdsonics@yahoo.com>
2024-02-21 05:25:31 -0500

    beginning on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,14 @@
 
 import React from 'react';
 import '../assets/css/index.css';
-
+import '../assets/css/home.css';
 
 function Home() {
-  return <h1>Home Page</h1>;
+  return <div>
+    <h1>Home Page</h1>
+    <h2> Impacting your community!</h2>
+    
+  </div>;
 }
 
 export default Home;
