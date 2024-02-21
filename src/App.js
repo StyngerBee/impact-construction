683a47e895033eb5adf940d5f36f317780f200bc
StyngerBee <cdsonics@yahoo.com>
2024-02-21 02:34:02 -0500

    fixed nav listing order

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import About from './pages/About';
 import Header from './components/header/Header';
 import Work from './pages/Work';
 import Contact from './pages/Contact';
@@ -11,11 +10,10 @@ function App() {
   <>
   <Header/>
   <Routes>
-    <Route path='/impact-construction' element={<Home />} />
-    {/* <Route path='/About' element={<About />}/> */}
-    <Route path='/impact-construction/Work' element={<Work />}/>
-    <Route path='/impact-construction/Contact' element={<Contact />}/>
-    <Route path='/impact-construction/Testimonials' element={<Testimonials />}/>
+    <Route path='/' element={<Home />} />
+    <Route path='/Work' element={<Work />}/>
+    <Route path='/Contact' element={<Contact />}/>
+    <Route path='/Testimonials' element={<Testimonials />}/>
   </Routes>
   </>
   )
