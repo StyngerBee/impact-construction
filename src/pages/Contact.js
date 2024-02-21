5eda2178a7a4287b183d6ea79c3bbe95379fe7af
StyngerBee <cdsonics@yahoo.com>
2024-02-21 01:35:20 -0500

    fixed contact us map

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,9 +4,21 @@ import '../assets/css/index.css';
 
 function Contact() {
   return <div>
-  <h1>Contact Us</h1>
-  [um-iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d121189.43710335952!2d-65.00485403475565!3d18.339531352858614!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sPO%20box%209196%20St.%20Thomas%2000801%20VI!5e0!3m2!1sen!2sus!4v1708426289166!5m2!1sen!2sus" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"]
-  </div>;
+    <h1>Contact Us</h1>
+    <div className="google-map-code">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d121189.43710335952!2d-65.00485403475565!3d18.339531352858614!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sPO%20box%209196%20St.%20Thomas%2000801%20VI!5e0!3m2!1sen!2sus!4v1708426289166!5m2!1sen!2sus"
+        width="400"
+        height="300"
+        frameborder="0"
+        style={{ border: 0 }}
+        allowfullscreen=""
+        loading="lazy"
+        aria-hidden="false"
+        tabindex="0"
+        referrerpolicy="no-referrer-when-downgrade"
+      />
+    </div>  </div>;
 }
 
 export default Contact;
